47146407cd485dbc909f08e34254149026d0a5f0
boo <boo@boo.boo>
2015-11-17 07:13:14 -0800

    Add three page link

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-index.js
+console.log("YOO")
+
+var forms = document.querySelector('form');
+
+forms.addEventListener('submit', function(e) {
+	
+	console.log("I've been submitted")
+
+	var put = document.getElementsByTagName('input')[name='first-name'].value;
+	console.log(put)
+
+})
